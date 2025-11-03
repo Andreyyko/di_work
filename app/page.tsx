5e06dcae3fd22c3d5714ed611e01d246a0e7d606
overtimeEnjoyer <overtimeenjoyer@gmail.com>
2025-11-03 15:29:20 +0200

    fix deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <section>
-      <h1>HEllo World!</h1>
-      <Button > Hello bro</Button>
+    <section className="flex min-h-screen items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">Hello World!</h1>
+        <Button>Hello bro</Button>
+      </div>
     </section>
   );
 }
