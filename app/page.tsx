cdddb0abd947a7043a117c8e93bfc2869041504b
vasyllendiel <gesundheitqq@gmail.com>
2025-11-03 13:53:39 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section>
+      <h1>HEllo World!</h1>
+      <Button > привіт</Button>
+    </section>
+  );
+}
