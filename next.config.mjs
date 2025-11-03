cf1b1cb267abb5c28e425be0771fdafee34d9dcd
overtimeEnjoyer <overtimeenjoyer@gmail.com>
2025-11-03 15:44:53 +0200

    fix some problem with build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.mjs
+const nextConfig = {
+  reactStrictMode: true,
 };
 
 export default nextConfig;
