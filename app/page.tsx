7a87f9631ea87a64fd16ee340a30f0f49d87e9b6
overtimeEnjoyer <overtimeenjoyer@gmail.com>
2025-11-07 15:51:25 +0200

    check fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 
+import IdentityIntro  from "@/components/IdentityIntroSection/IdentityIntro";
+
 export default function Home() {
   return (
     <section className="flex flex-col min-h-screen items-center justify-center">
       
+      <IdentityIntro />
     </section>
   );
 }
---
+++ components/IdentityIntroSection/IdentityIntro.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import FontHeaderTwo from "../common/FontHeaderTwo";
+
+const IdentityIntro: React.FC = () => {
+  return (
+    <section className="w-full px-6 py-12">
+      <div className="flex flex-col md:flex-row items-start md:items-center md:justify-center gap-6">
+        <div className="">
+          <img
+            src="/your-image.jpg"
+            alt="Відкрита книга"
+            className="w-40 h-auto"
+          />
+        </div>
+
+        <div className="max-w-4xl text-center md:text-left space-y-4">
+          <FontHeaderTwo
+            text="ПЕРШИЙ В УКРАЇНІ ПСИХОЛОГІЧНИЙ"
+            highlights={[
+              {
+                id: 0,
+                className: "font-alexandra text-brand-bordo",
+                size: "140px",
+              },
+            ]}
+            className=""
+          />
+
+          <FontHeaderTwo
+            text="САЙТ, ЩО ПОЄДНУЄ РІЗНІ НАПРЯМКИ ДОКАЗОВОЇ"
+            highlights={[
+              {
+                id: 0,
+                className: "font-alexandra text-brand-bordo",
+                size: "140px",
+              },
+              {
+                id: 0,
+                className: "font-alexandra text-brand-bordo",
+                size: "140px",
+              },
+            ]}
+            className=""
+          />
+
+          <FontHeaderTwo
+            text="ПСИХОТЕРАПІЇ, СПРЯМОВАНИЙ"
+            highlights={[
+              {
+                id: 0,
+                className: "font-alexandra text-brand-bordo",
+                size: "140px",
+              },
+            ]}
+            className=""
+          />
+
+          <FontHeaderTwo
+            text="НА ПІДТРИМКУ ПСИХІЧНОГО ЗДОРОВ’Я ТА ВСІХ СФЕР ОСОБИСТОСТІ"
+            highlights={[
+              {
+                id: 0,
+                className: "font-alexandra text-brand-bordo",
+                size: "140px",
+              },
+              {
+                id: 0,
+                className: "font-alexandra text-brand-bordo",
+                size: "140px",
+              },
+            ]}
+            className=" "
+          />
+
+          <p className="text-sm md:text-base font-medium mt-3">
+            Ресурсно-орієнтований когнітивно поведінковий
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default IdentityIntro;
