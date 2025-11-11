9847f3aefb050bb6db66c4b05efc4f523fed812a
Andreyyko <oles.andreyko@gmail.com>
2025-11-11 12:58:06 +0200

    change frame-wrapper element

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 
 
 import HeroSection from "@/components/HeroSection";
+import FrameWrapper from "@/components/common/FrameWrapper";
+import { gold_frame } from "@/public/images/GoldFrameImage";
 
 export default function Home() {
   return (
-    <section className="">
+    <section className="bg-[#eeece5]">
       <HeroSection/>
     </section>
   );
--- public/images/GoldFrameImage/index.ts
+++ public/images/GoldFrameImage/index.ts
@@ -1,5 +1,5 @@
 import GOLD_FRAME from './goldFrame.svg'
-import PHOTO from './Rectangle 79.png'
+import PHOTO from './Rectangle 77.svg'
 
 export const gold_frame = {
     GOLD_FRAME,
