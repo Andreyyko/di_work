ee234d7eb5be6da0e2b0f6778adb4dfb18c74011
overtimeEnjoyer <overtimeenjoyer@gmail.com>
2025-11-21 18:11:23 +0200

    change font component on nurgermenu.tsx

--- components/common/Header/BurgerMenu.tsx
+++ components/common/Header/BurgerMenu.tsx
@@ -156,7 +156,7 @@ const BurgerMenu = ({ onClose, isMobile, closing }: Props) => {
         >
           <div className="flex flex-col items-center gap-5 sm:gap-7.5">
             <button className="heading-2-burger uppercase menu-item menu-fade">
-              <span className="first-letter-burger">Про нас</span>
+              <span className="first-letter-burger">П</span>ро нас
               <span className="heading-burger pl-2 sm:pl-5">01</span>
             </button>
 
